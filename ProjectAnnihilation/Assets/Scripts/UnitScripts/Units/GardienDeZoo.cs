fb82190e03c8460cb2cec5c2fc8e965a75716951
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-01-23 23:59:46 +0100

    gardien de zoo done

---
+++ ProjectAnnihilation/Assets/Scripts/UnitScripts/Units/GardienDeZoo.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class GardienDeZoo : Unit
+{
+    [Header("Gardien de Zoo")]
+    [SerializeField]
+    private ProjectileLauncher launcher;
+    [Header("Darts")]
+    [SerializeField]
+    private int nDarts;
+    //[SerializeField]
+    //private float damagePerDart;
+    [SerializeField]
+    private float dartSpeed;
+    [SerializeField]
+    private float dartRange;
+    [SerializeField]
+    private float timeBetweenDarts;
+
+    [Header("Trap")]
+    [SerializeField]
+    private float trap; // To replace with trap
+
+    protected override bool Action(GameObject target = null)
+    {
+        if (!base.Action(target))
+            return false;
+
+        if (target == null)
+            return false;
+
+        StartCoroutine(LaunchSeveralBullets(target, nDarts, timeBetweenDarts));
+
+        return true;
+    }
+
+    protected override bool SpecialAction(GameObject target = null)
+    {
+        if (!base.SpecialAction(target))
+            return false;
+
+        return true;
+    }
+
+    private IEnumerator LaunchSeveralBullets(GameObject target, int nBullets, float dT)
+    {
+        DamageData dd = new(unitData.Attack/nBullets);
+
+        for (int i = 0; i < nBullets; i++)
+        {
+            launcher.LaunchRect((target.transform.position - transform.position).normalized * dartSpeed, dd, dartRange / dartSpeed);
+            yield return new WaitForSeconds(dT);
+        }
+    }
+}
