a524eb64bfa9a54c3428a859e0deed426cb5dc0c
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-01-23 12:09:45 +0100

    script clean up

--- ProjectAnnihilation/Assets/Scripts/MapScripts/Sand.cs
+++ ProjectAnnihilation/Assets/Scripts/MapScripts/Sand.cs
@@ -1,9 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.XR;
-using UnityEngine;
-using UnityEngine.AI;
-
 public class Sand : Tile
 {
     // Start is called before the first frame update
